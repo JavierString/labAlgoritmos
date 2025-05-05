fd95ece995c78dec6dfeb5ada408f1600e25c4ed
Javier Contreras <javiercl180906@gmail.com>
2025-05-05 14:59:21 -0600

    cambio listaMedicamentos

--- ListaMedicamentos.java
+++ ListaMedicamentos.java
@@ -1,4 +1,16 @@
 // lista de los medicamentos
 public class ListaMedicamentos{
-
+    private NodoMedicamentos cabeza;
+    public void insertarMedicamento(String codigo, String nombre, String fechaVencimiento, int cantidadDisponible){
+        NodoMedicamentos nuevo = new NodoMedicamentos(codigo, nombre, fechaVencimiento, cantidadDisponible);
+        
+        if(cabeza == null){
+        cabeza = nuevo;
+        cabeza.siguiente = cabeza;
+        cabeza.anterior = cabeza;
+        return;
+        }
+        NodoMedicamentos actual = cabeza;
+        
+        }
 }
